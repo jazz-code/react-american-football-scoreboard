34e587275efe8e4adf5b94b9219bc2aaf8ee3ae7
jazz-code <ren.meta@gmail.com>
2019-07-15 17:42:45 -0400

    starting the React Timer with the useEffect Hook

--- src/Timer.js
+++ src/Timer.js
@@ -8,6 +8,23 @@ const Timer = () => {
     setIsActive(!isActive);
   }
 
+  function reset() {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
   return (
     <div className="timer">
       00:03
