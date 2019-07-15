d28ed29722fe0e37b5c10165c9c5df56f27382e1
jazz-code <ren.meta@gmail.com>
2019-07-15 17:38:46 -0400

    fixed out of place /div, added function toggle

--- src/Timer.js
+++ src/Timer.js
@@ -3,15 +3,28 @@ import React, { useState, useEffect } from "react";
 const Timer = () => {
   const [seconds, setSeconds] = useState(0);
   const [isActive, setIsActive] = useState(false);
-};
 
-return (
-    <div className="timer">00:03</div>
-    <div className="time">{seconds}s</div>
-    <div className="row">
-        <button className={`buttons button-primary-${isActive ? 'active' : 'inactive'}`} onClick={toggle}>
-            {isActive ? 'Pause' : 'Start'}
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  return (
+    <div className="timer">
+      00:03
+      <div className="time">{seconds}s</div>
+      <div className="row">
+        <button
+          className={`buttons button-primary-${
+            isActive ? "active" : "inactive"
+          }`}
+          onClick={toggle}
+        >
+          {isActive ? "Pause" : "Start"}
         </button>
-        <button className="buttons" onClick={reset}>Reset</button>
+        <button className="buttons" onClick={reset}>
+          Reset
+        </button>
+      </div>
     </div>
-);
+  );
+};
